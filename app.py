4249e15029314a6f6e7e93e684e6c89e20b932b2
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-09 09:02:17 +0100

    created routes for other pages

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Home route
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# About route
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
+# Product route 
+@app.route("/product")
+def index():
+    return render_template("upload.html")
+
+# Help route
+@app.route("/help")
+def index():
+    return render_template("help.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
