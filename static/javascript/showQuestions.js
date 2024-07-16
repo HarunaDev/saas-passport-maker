8ac19633a92d04fe5d6dbaee045648a1ce91509d
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-16 12:35:05 +0100

    created function to display help template content
    created an array with objects inside of it, they hold key-value pairsor questions and answers

--- static/javascript/showQuestions.js
+++ static/javascript/showQuestions.js
@@ -1,3 +1,24 @@
 const faq = document.getElementById("FAQ")
 
-faq.innerHTML = `<h1>This is the help Section</h1>`
+// faq.innerHTML = `<h1>This is the help Section</h1>`
+
+// function to generate content in the help template
+function showQuestions() {
+    // create array with object to store faq and answers
+    const questionArr = [
+        {
+          question_title: "Can I stream movies on my watch-list",
+          question_text: "No, you can't but it is a feature that will be implemented in later versions"
+        },
+        {
+          question_title: "Can I share my watch-list",
+          question_text: "Yes, you can share watch list on different platforms for others to see."
+        },
+        {
+          question_title: "Are there any ads?",
+          question_text: "No, the app is ad free, so you can enjoy a wonderful experience without any interruption."
+        }
+    ];
+
+
+}
