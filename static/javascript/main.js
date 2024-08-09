397746f7c8163ea55ed1a04cf218c415e528ccb7
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-09 18:17:34 +0100

    added javascript code to make button functional, but experiencing bug

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -9,3 +9,33 @@ if ("serviceWorker" in navigator) {
         })
     })
   }
+
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('beforeinstallprompt event fired');
+    e.preventDefault();
+    deferredPrompt = e;
+    const installButton = document.getElementById('install-button');
+    installButton.style.display = 'block';
+
+    installButton.addEventListener('click', () => {
+        console.log('Install button clicked');
+        installButton.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            console.log('User choice: ', choiceResult.outcome);
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            } else {
+                console.log('User dismissed the install prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+window.addEventListener('appinstalled', () => {
+    console.log('PWA was installed');
+    document.getElementById('install-button').style.display = 'none';
+});
