0371e841573153f50f5db47c50e9330e1503f002
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-11 13:19:48 +0100

    moved upload button code to another script

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,26 +1,13 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("./static/javascript/sw.js").then(registration => {
-            console.log("SW Registered!")
-            console.log(registration);
-        }).catch(error => {
-            console.log("Sw Registration Failed")
-            console.log(error)
+document.addEventListener('DOMContentLoaded', () => {
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker.register("./static/javascript/sw.js").then(registration => {
+                console.log("SW Registered!")
+                console.log(registration);
+            }).catch(error => {
+                console.log("Sw Registration Failed")
+                console.log(error)
+            })
         })
-    })
-  }
-
-// Upload button 
-document.getElementById('upload-button').addEventListener('click', function() {
-    // Trigger the hidden file input field
-    document.getElementById('file-input').click();
-});
-
-// Optional: Handle the file selection
-document.getElementById('file-input').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        console.log('File selected:', file.name);
-        // You can proceed to upload the file or display a preview, etc.
     }
-});
+})
